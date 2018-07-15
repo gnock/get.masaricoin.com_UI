968a4e3cb565ec26218d38e3f8404ca78c193a3d
TOGM <36015720+TOGM@users.noreply.github.com>
2018-07-15 13:55:56 -0600

    change for testnet

--- app/user/home/home.js
+++ app/user/home/home.js
@@ -50,7 +50,7 @@ app.controller('HomeCtrl', function($scope, $route, dataService, timerService) {
         });
 		
 		
-		        dataService.getData("/pool/chart/hashrate/pplns", function(data){
+		        dataService.getData("/pool/chart/networkHashrate", function(data){
 				data = _.forEach(data, function(element) {
                 element.ts = new Date(element.ts);
 				element.hs = (element.diffficulty / 120) / 1000;
