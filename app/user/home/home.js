a3afa302aa2aecc09a72ee7c319b789b70444ff0
TOGM <36015720+TOGM@users.noreply.github.com>
2018-07-17 09:08:30 -0600

    Testing Interpolation

--- app/user/home/home.js
+++ app/user/home/home.js
@@ -113,6 +113,10 @@ app.controller('HomeCtrl', function ($scope, $route, dataService, timerService) 
 						"id": "global",
 						"dataset": "global",
 						"label": "Total Network Hashrate",
+						"interpolation": {
+								mode: "bundle",
+								tension: 1
+							},
 						"key": "hs",
 						"color": "green",
 						"type": ["line", "area"]
