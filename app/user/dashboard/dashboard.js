76eefb233ca9bcadb828a920fd68e7a5be1fa0cb
TOGM <36015720+TOGM@users.noreply.github.com>
2018-07-17 01:48:47 -0600

    Code Beautify

--- app/user/dashboard/dashboard.js
+++ app/user/dashboard/dashboard.js
@@ -1,22 +1,22 @@
 'use strict';
 
-app.controller('DashboardCtrl', function($scope , $route, $mdDialog, $pageVisibility, dataService, timerService, addressService, minerService) {
-	
+app.controller('DashboardCtrl', function ($scope, $route, $mdDialog, $pageVisibility, dataService, timerService, addressService, minerService) {
+
 	$scope.profit = {
 		'hashrate': '',
 		'hashes': '',
-		'effort':'',
-		'current_effort':'',
-   }
-   
-	$scope.Update = function() {
-		
+		'effort': '',
+		'current_effort': '',
+	}
+
+	$scope.Update = function () {
+
 		var attr = $scope.profit;
 		var OneHash = document.getElementById("OneHash").innerHTML
 		var msrtousd = document.getElementById("msrusd").innerHTML
 		var msrtoeur = document.getElementById("msreur").innerHTML
 		var msrtogbp = document.getElementById("msrgbp").innerHTML
-		
+
 		var msrhour = OneHash * attr['hashrate'] * attr['hashes'];
 		var msrday = OneHash * attr['hashrate'] * attr['hashes'] * 24;
 		var usdhour = OneHash * attr['hashrate'] * attr['hashes'] * msrtousd;
@@ -25,8 +25,8 @@ app.controller('DashboardCtrl', function($scope , $route, $mdDialog, $pageVisibi
 		var eurday = OneHash * attr['hashrate'] * attr['hashes'] * 24 * msrtoeur;
 		var gbphour = OneHash * attr['hashrate'] * attr['hashes'] * msrtogbp;
 		var gbpday = OneHash * attr['hashrate'] * attr['hashes'] * 24 * msrtogbp;
-		
-		
+
+
 		document.getElementById("msrhour").innerHTML = msrhour.toFixed(6);
 		document.getElementById("msrday").innerHTML = " (" + msrday.toFixed(6) + ")";
 		document.getElementById("usdhour").innerHTML = usdhour.toFixed(2);
@@ -38,86 +38,84 @@ app.controller('DashboardCtrl', function($scope , $route, $mdDialog, $pageVisibi
 	}
 
 	$scope.minerStats = {};
-	
-	$scope.updateCharts = function (){
-			
-			minerService.updateStats($scope.addrStats, function(minerStats){
+
+	$scope.updateCharts = function () {
+
+		minerService.updateStats($scope.addrStats, function (minerStats) {
 			$scope.minerStats = minerStats;
-			
-			
-			
+
+
 		});
 	}
 
 	// Update miners everyime addrStats
-	$scope.$parent.$watch('addrStats', function(newValue, oldValue) {
+	$scope.$parent.$watch('addrStats', function (newValue, oldValue) {
 		$scope.updateCharts();
 	});
-	
-	$scope.addAddress = function (){
-		if ($scope.paymentAddress){
+
+	$scope.addAddress = function () {
+		if ($scope.paymentAddress) {
 			addressService.trackAddress($scope.paymentAddress);
 			$scope.paymentAddress = "";
 		}
 	};
-	
-	$scope.deleteAddress = function (key, ev){
+
+	$scope.deleteAddress = function (key, ev) {
 		var confirm = $mdDialog.confirm()
-		.title('Hide live stats?')
-		.textContent('You can add it back by entering your wallet address again')
-		.ariaLabel('Stop tracking payment address')
-		.targetEvent(ev)
-		.ok("Remove")
-		.cancel("Cancel");
-		
-		$mdDialog.show(confirm).then(function() {
+			.title('Hide live stats?')
+			.textContent('You can add it back by entering your wallet address again')
+			.ariaLabel('Stop tracking payment address')
+			.targetEvent(ev)
+			.ok("Remove")
+			.cancel("Cancel");
+
+		$mdDialog.show(confirm).then(function () {
 			addressService.deleteAddress(key);
-		}, function() {
+		}, function () {
 			// cancel do nothing
 		});
 	}
 
-	$scope.setAlarm = function(addr, bool){
+	$scope.setAlarm = function (addr, bool) {
 		addressService.setAlarm(addr, bool);
 	};
-	
-	
 
-	$scope.viewPayments = function(ev, miner, addr){
-		
+
+	$scope.viewPayments = function (ev, miner, addr) {
+
 		$mdDialog.show({
-			locals: {
-				miner: miner,
-				addr: addr
-			},
-			controller: "MinerPaymentsCtrl",
-			templateUrl: 'user/dashboard/minerpayments.html',
-			parent: angular.element(document.body),
-			targetEvent: ev,
-			clickOutsideToClose:true,
-			fullscreen: !$scope.menuOpen
-		})
-		.then(function(answer) {
-			$scope.status = 'You said the information was "' + answer + '".';
-		}, function() {
-			$scope.status = 'You cancelled the dialog.';
-		});
+				locals: {
+					miner: miner,
+					addr: addr
+				},
+				controller: "MinerPaymentsCtrl",
+				templateUrl: 'user/dashboard/minerpayments.html',
+				parent: angular.element(document.body),
+				targetEvent: ev,
+				clickOutsideToClose: true,
+				fullscreen: !$scope.menuOpen
+			})
+			.then(function (answer) {
+				$scope.status = 'You said the information was "' + answer + '".';
+			}, function () {
+				$scope.status = 'You cancelled the dialog.';
+			});
 	}
-	
-	
+
 
 	// Recurring API calls and timer
 	var loadData = function () {
-		_.each($scope.poolList, function(pool_type) {
-			dataService.getData("/pool/stats/"+pool_type, function(data){
+		_.each($scope.poolList, function (pool_type) {
+			dataService.getData("/pool/stats/" + pool_type, function (data) {
 				$scope.poolStats[pool_type] = data;
 			});
-			
 
-			dataService.getData("/pool/blocks/"+pool_type, function(data){
-				if (data.length > 0){
+
+			dataService.getData("/pool/blocks/" + pool_type, function (data) {
+				if (data.length > 0) {
 					$scope.lastBlock[pool_type] = data[0];
-				} else {
+				}
+				else {
 					$scope.lastBlock[pool_type] = {
 						ts: 0,
 						hash: "",
@@ -132,28 +130,26 @@ app.controller('DashboardCtrl', function($scope , $route, $mdDialog, $pageVisibi
 				}
 			});
 		});
-		
+
 
 		// Call minerservice update
 		$scope.updateCharts();
 	};
 
 	// No spawn xhr reqs in bg
-	$pageVisibility.$on('pageFocused', function(){
+	$pageVisibility.$on('pageFocused', function () {
 		minerService.runService(true);
 	});
 
-	$pageVisibility.$on('pageBlurred', function(){
+	$pageVisibility.$on('pageBlurred', function () {
 		minerService.runService(false);
 	});
 
 	// Register call with timer 
-	timerService.register(loadData, $route.current.controller); 
+	timerService.register(loadData, $route.current.controller);
 	loadData();
-	
-	
-	
-	
+
+
 	$scope.$on("$routeChangeStart", function () {
 		timerService.remove($route.current.controller);
 	});
--- app/user/home/home.js
+++ app/user/home/home.js
@@ -1,60 +1,46 @@
 'use strict';
 
-app.controller('HomeCtrl', function($scope, $route, dataService, timerService) {
+app.controller('HomeCtrl', function ($scope, $route, dataService, timerService) {
 
-    function ticker() {
-        dataService.getData("/pool/chart/hashrate/pplns", function(data){
-		data = _.forEach(data, function(element) {
-                element.ts = new Date(element.ts);
-				element.hs = element.hs/1000;
-				
-            		});
+	function ticker() {
+		dataService.getData("/pool/chart/hashrate/pplns", function (data) {
+			data = _.forEach(data, function (element) {
+				element.ts = new Date(element.ts);
+				element.hs = element.hs / 1000;
 
-            $scope.poolHashrateChart = {
-                datasets: { global: data },
-                options: {
-/*				    scales: {
-						yAxes: [{
-							ticks: {
-								suggestedMin: 50,
-								suggestedMax: 10000000
-							}
-						}]
-					},
-					tooltips: {
-						enabled: true,
-						mode: 'single',
-						callbacks: {
-							label: function(tooltipItems, data) { 
-							return tooltipItems.yLabel + ' €';
-							}
+			});
+
+			$scope.poolHashrateChart = {
+				datasets: {
+					global: data
+				},
+				options: {
+
+					series: [{
+						"axis": "y",
+						"id": "global",
+						"dataset": "global",
+						"label": "Total Pool Hashrate",
+						"key": "hs",
+						"color": "green",
+						"type": ["line", "area"]
+					}],
+					allSeries: [],
+					axes: {
+						x: {
+							key: "ts",
+							type: "date"
 						}
-					},*/
-                    series: [
-                        {"axis":"y",
-						 "id":"global",
-						 "dataset":"global",
-						 "label":"Total Pool Hashrate",
-						 "key":"hs",
-						 "color":"green",
-						 "type":["line","area"]}
-                    ],
-                    allSeries: [],
-                    axes: {
-                        x: {
-                            key: "ts",
-                            type: "date"
-                        }
-                    }
-                }
-            }
-        });
-		
-			var end_amount = 20;
+					}
+				}
+			}
+		});
+
+		var end_amount = 20;
 		var start_amount = 1;
 		var payout_fee = 0.1;
-                var slewData = [];
-                var amount_interval = (end_amount - start_amount) / 50;
+		var slewData = [];
+		var amount_interval = (end_amount - start_amount) / 50;
 		var y_ticks = [payout_fee / 2, payout_fee];
 		var x_ticks = [
 			start_amount,
@@ -63,169 +49,186 @@ app.controller('HomeCtrl', function($scope, $route, dataService, timerService) {
 		];
 
 
-                for (var amount = start_amount; amount.toFixed(8) <= end_amount; amount += amount_interval) {
-                        var applied_fee = Math.max(0, 1 - (amount - start_amount) / (end_amount - start_amount)) * payout_fee;
-			slewData.push({amount: parseFloat(amount.toFixed(8)), fee: parseFloat(applied_fee.toFixed(8))});
-                }
+		for (var amount = start_amount; amount.toFixed(8) <= end_amount; amount += amount_interval) {
+			var applied_fee = Math.max(0, 1 - (amount - start_amount) / (end_amount - start_amount)) * payout_fee;
+			slewData.push({
+				amount: parseFloat(amount.toFixed(8)),
+				fee: parseFloat(applied_fee.toFixed(8))
+			});
+		}
 
 		$scope.feeSlewChart = {
-		    datasets: { global: slewData },
-		    options: {
-		        series: [
-		            {"axis":"y","id":"global","dataset":"global","label":"Transaction Fee","key":"fee","color":"green","type":["line","area"]}
-		        ],
-		        allSeries: [],
-		        axes: {
-		            x: {
-		                key: "amount",
-		                type: "linear",
-		                ticks: x_ticks,
-		                includeZero: true,
-		                tickFormat: function(value, index) {
-		                    return value + " MSR";
-		                }
-		            },
-		            y: {
-		                ticksShift: {y: 3.5, x: 0},
-		                ticks: y_ticks,
-		            }
-		        }
-		    }
+			datasets: {
+				global: slewData
+			},
+			options: {
+				series: [{
+					"axis": "y",
+					"id": "global",
+					"dataset": "global",
+					"label": "Transaction Fee",
+					"key": "fee",
+					"color": "green",
+					"type": ["line", "area"]
+				}],
+				allSeries: [],
+				axes: {
+					x: {
+						key: "amount",
+						type: "linear",
+						ticks: x_ticks,
+						includeZero: true,
+						tickFormat: function (value, index) {
+							return value + " MSR";
+						}
+					},
+					y: {
+						ticksShift: {
+							y: 3.5,
+							x: 0
+						},
+						ticks: y_ticks,
+					}
+				}
+			}
 		};
-		
-		
-		
-		        dataService.getData("/pool/chart/networkHashrate", function(data){
-				data = _.forEach(data, function(element) {
+
+
+		dataService.getData("/pool/chart/networkHashrate", function (data) {
+			data = _.forEach(data, function (element) {
 				element.ts = element.ts * 1000;
-                element.ts = new Date(element.ts);
+				element.ts = new Date(element.ts);
 				element.hs = element.difficulty / 120;
 				element.hs = element.difficulty / 1000;
-            		});
+			});
 
-            $scope.NetworkHashrateChart = {
-                datasets: { global: data },
-                options: {
+			$scope.NetworkHashrateChart = {
+				datasets: {
+					global: data
+				},
+				options: {
 
-                    series: [
-                        {"axis":"y",
-						 "id":"global",
-						 "dataset":"global",
-						 "label":"Total Network Hashrate",
-						 "key":"hs",
-						 "color":"green",
-						 "type":["line","area"]}
-                    ],
-                    allSeries: [],
-                    axes: {
-                        x: {
-                            key: "ts",
-                            type: "date"
-                        }
-                    }
-                }
-            }
-        });
-    }
+					series: [{
+						"axis": "y",
+						"id": "global",
+						"dataset": "global",
+						"label": "Total Network Hashrate",
+						"key": "hs",
+						"color": "green",
+						"type": ["line", "area"]
+					}],
+					allSeries: [],
+					axes: {
+						x: {
+							key: "ts",
+							type: "date"
+						}
+					}
+				}
+			}
+		});
+	}
 
-    function ticker2() {
-        dataService.getData("/pool/blocks", function(data){
-	    var i = 25;
-            data = _.forEach(data, function(element) {
-//                element.ts = new Date(element.ts);
-		element.value = element.shares/(element.diff/100)
-		element.ts = i;
-		i-=1;
-            });
- 
-            $scope.poolEffortChart = {
-                datasets: { global: data },
-                options: {
-                    series: [
-                        {"axis":"y","id":"global","dataset":"global","label":"Effort","interpolation":{mode: "bundle", tension: 1},"key":"value","color":"green","type":["line"]},
-			{"axis":"y","id":"global1","dataset":"global","label":"Effort","key":"value","color":"green","type":["dot"]}
-                    ],
-                    allSeries: [],
-                    axes: {
-                        x: {
-                            key: "ts"
-//                            type: "date"
-                        }
-                    }
-                }
-            }
-        });
-    }
+	function ticker2() {
+		dataService.getData("/pool/blocks", function (data) {
+			var i = 25;
+			data = _.forEach(data, function (element) {
+				//                element.ts = new Date(element.ts);
+				element.value = element.shares / (element.diff / 100)
+				element.ts = i;
+				i -= 1;
+			});
 
-    function ticker3() {
-        dataService.getData("/pool/chart/miners", function(data){
+			$scope.poolEffortChart = {
+				datasets: {
+					global: data
+				},
+				options: {
+					series: [{
+							"axis": "y",
+							"id": "global",
+							"dataset": "global",
+							"label": "Effort",
+							"interpolation": {
+								mode: "bundle",
+								tension: 1
+							},
+							"key": "value",
+							"color": "green",
+							"type": ["line"]
+						},
+						{
+							"axis": "y",
+							"id": "global1",
+							"dataset": "global",
+							"label": "Effort",
+							"key": "value",
+							"color": "green",
+							"type": ["dot"]
+						}
+					],
+					allSeries: [],
+					axes: {
+						x: {
+							key: "ts"
+							//                            type: "date"
+						}
+					}
+				}
+			}
+		});
+	}
 
-            data = _.forEach(data, function(element) {
-                element.ts = new Date(element.ts);
-            });
- 
-            $scope.poolMinersChart = {
-                datasets: { global: data },
-                options: {
-                    series: [
-                        {"axis":"y","id":"global","dataset":"global","label":"Total Pool Miners","key":"cn","color":"green","type":["line","area"]}
-                    ],
-                    allSeries: [],
-                    axes: {
-                        x: {
-                            key: "ts",
-                            type: "date"
-                        }
-                    }
-                }
-            }
-        });
-    }
-	        			//var MSR = 0;
-						/*
-						var MSR = {};
-						$.ajax({
-        				url: "https://www.southxchange.com/api/price/msr/usd",
-        				async: false,
-        				dataType: 'json',
-        				success: function(data) {
-            				MSR = data.Last;
-        				}
-    				});
-					
-						//var BTC = 0;
-						
-						var BTC = {};
-    					$.ajax({
-        				url: "https://www.southxchange.com/api/price/msr/btc",
-        				async: false,
-        				dataType: 'json',
-        				success: function(data) {
-            				BTC = data.Last;
-        				}
-    				});
-					
-				$scope.aeonusd = (MSR * 1).toFixed(4);
-				$scope.msrbtc = (BTC * 1).toFixed(8);
-				*/
-    timerService.register(ticker, 'poolHashrateChart');
-    ticker();
+	function ticker3() {
+		dataService.getData("/pool/chart/miners", function (data) {
 
-    timerService.register(ticker2, 'poolMinersChart');
-    ticker2();
+			data = _.forEach(data, function (element) {
+				element.ts = new Date(element.ts);
+			});
 
-    timerService.register(ticker3, 'poolEffortChart');
-    ticker3();
+			$scope.poolMinersChart = {
+				datasets: {
+					global: data
+				},
+				options: {
+					series: [{
+						"axis": "y",
+						"id": "global",
+						"dataset": "global",
+						"label": "Total Pool Miners",
+						"key": "cn",
+						"color": "green",
+						"type": ["line", "area"]
+					}],
+					allSeries: [],
+					axes: {
+						x: {
+							key: "ts",
+							type: "date"
+						}
+					}
+				}
+			}
+		});
+	}
+	timerService.register(ticker, 'poolHashrateChart');
+	ticker();
 
-    $scope.$on("$routeChangeStart", function () {
-        timerService.remove("poolHashrateChart");
-    });
+	timerService.register(ticker2, 'poolMinersChart');
+	ticker2();
 
-    $scope.$on("$routeChangeStart", function () {
-        timerService.remove("poolMinersChart");
-    });
+	timerService.register(ticker3, 'poolEffortChart');
+	ticker3();
 
-    $scope.$on("$routeChangeStart", function () {
-        timerService.remove("poolEffortChart");
-    });
+	$scope.$on("$routeChangeStart", function () {
+		timerService.remove("poolHashrateChart");
+	});
+
+	$scope.$on("$routeChangeStart", function () {
+		timerService.remove("poolMinersChart");
+	});
+
+	$scope.$on("$routeChangeStart", function () {
+		timerService.remove("poolEffortChart");
+	});
 });
